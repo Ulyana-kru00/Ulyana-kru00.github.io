756f2cd3cf3a21f2d2bade61b07b42241d553e5a
Ulyana-kru00 <167925031+Ulyana-kru00@users.noreply.github.com>
2024-05-29 22:33:31 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,8 +14,21 @@ switchMode.onclick = function(){
     localStorage.setItem('theme', theme.href); 
 }
 
-Document.addEventListener("DOMContentLoaded", function(event) { 
-    // Скрываем анимацию и показываем контент после загрузки страницы 
-    document.querySelector('.loader-wrapper').style.display = 'none'; 
-    document.querySelector('.content').style.display = 'block'; 
-  });
+let switchMode = document.getElementById("switchMode");
+switchMode.onclick = function(){
+    let theme = document.getElementById("theme");
+    if(theme.getAttribute("href") == "styles/style.css"){
+        theme.href = "styles/secondVar.css";
+    }
+    else{
+        theme.href = "styles/style.css";
+    }
+}
+
+window.onload = function(){
+    let preloader = document.getElementById ('preloader');
+    preloader.classList.add('hide-preloader');
+    setTimeout(function() {
+        preloader.classList.add('preloader-hidden');
+    },950)
+}
