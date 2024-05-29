ac0aa985889f76afada084433bb2f3b71916d01d
Ulyana-kru00 <167925031+Ulyana-kru00@users.noreply.github.com>
2024-05-29 23:32:05 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,17 +14,6 @@ switchMode.onclick = function(){
     localStorage.setItem('theme', theme.href); 
 }
 
-let switchMode = document.getElementById("switchMode");
-switchMode.onclick = function(){
-    let theme = document.getElementById("theme");
-    if(theme.getAttribute("href") == "styles/style.css"){
-        theme.href = "styles/secondVar.css";
-    }
-    else{
-        theme.href = "styles/style.css";
-    }
-}
-
 window.onload = function(){
     let preloader = document.getElementById ('preloader');
     preloader.classList.add('hide-preloader');
